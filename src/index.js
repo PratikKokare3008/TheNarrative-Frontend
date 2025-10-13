ec53a7420dc751431d5a02a9b45a9eb8827fc521
PratikKokare3008 <wordsbypratik@gmail.com>
2025-10-14 00:33:58 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -188,3 +188,4 @@ if (typeof window !== 'undefined') {
     window.performance.measure('app-load-time', 'app-start', 'app-loaded');
   });
 }
+
