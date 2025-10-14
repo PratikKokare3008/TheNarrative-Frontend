59a78d434e58276307772870a5c1c5773dbd0c5a
PratikKokare3008 <wordsbypratik@gmail.com>
2025-10-14 11:32:59 +0530

    Update SportsSchedule.js

--- src/components/SportsSchedule.js
+++ src/components/SportsSchedule.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useCallback, useMemo } from 'react'; // FIXED: Removed unused useEffect import
 import { useQuery } from 'react-query';
 import axios from 'axios';
 import moment from 'moment';
